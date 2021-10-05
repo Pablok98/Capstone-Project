55fbba96888f248c6bdeebd25c3865c5e75cced6
Luis Felipe Tapia <lfelipe.tapia@uc.cl>
2021-10-05 14:29:25 -0300

    nada

--- src/math/main.py
+++ src/math/main.py
@@ -5,7 +5,7 @@ from calidad_esperada_sim import *
 from markov import stationary_distribution
 
 # Aquí probamos todos los archivos de /math
-for p_11, p_01 in ((0.3, 0.2), (0.4, 0.3), (0.5, 0.2), (0.6, 0.3), (0.9, 0.1)):
+for p_11, p_01 in ((0.3, 0.2), (0.4, 0.3), (0.6, 0.3), (0.9, 0.1), (0.9, 0.9)):
     m_transicion = np.array([[p_11, (1 - p_11)], [p_01, (1 - p_01)]])
     p_lluvia = stationary_distribution(m_transicion)[0]
     f_optimal_q = optimal_q_generator(0.85, 0.95)
