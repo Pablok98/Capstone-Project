b0efdd6541f6d9bd0db6ff8a2adb15653dde9602
Luis Felipe Tapia <lfelipe.tapia@uc.cl>
2021-10-05 11:53:12 -0300

    cambio a Markov para la sim

--- src/math/main.py
+++ src/math/main.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from calidad_esperada import *
 from calidad_esperada_sim import *
+from markov import stationary_distribution
 
 # Aquí probamos todos los archivos de /math
-for p_lluvia in (0.1, 0.25, 0.5, 0.75):
+for p_11, p_01 in ((0.3, 0.2), (0.4, 0.3), (0.5, 0.2), (0.6, 0.3), (0.9, 0.1)):
+    m_transicion = np.array([[p_11, (1 - p_11)], [p_01, (1 - p_01)]])
+    p_lluvia = stationary_distribution(m_transicion)[0]
     f_optimal_q = optimal_q_generator(0.85, 0.95)
     resultados_analiticos = [E_quality(t, p_lluvia, f_optimal_q, mu) for t in range(14)]
-    resultados_simulacion = run_sim(100000, 14, p_lluvia, mu, f_optimal_q)
+    resultados_simulacion = run_sim(100000, 14, m_transicion, mu, f_optimal_q)
 
     print(f'probabilidad lluvia: {p_lluvia}')
     print(*[f'dia {i}: {resultados_analiticos[i] - resultados_simulacion[i]}' for i in range(14)], sep='\n')
