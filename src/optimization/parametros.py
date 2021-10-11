0fdd836ad1e073b8b9f77c3ca639b93b5e62ebb4
Luis Felipe Tapia <lfelipe.tapia@uc.cl>
2021-10-11 16:12:41 -0300

    formato de parametros

--- src/optimization/parametros.py
+++ src/optimization/parametros.py
@@ -6,12 +6,12 @@ import pandas as pd
 L = [0,1,2,3,4,5,6]
 T = [0,1,2,3,4,5,6]
 K = [i for i in range(100)]
-C = [i for i in range(20)]#Camiones desde 0 hasta 25 -> 7A, 3B, 8C, 7D
+C = [i for i in range(20)]  #Camiones desde 0 hasta 25 -> 7A, 3B, 8C, 7D
 P = [0,1,2,3,4]
 M = 1000
 
 ###############################################################################
-ef_cos = [[4000 * 10 for t in T] for l in L]#10 hras  ->  #kg/día                         #lt
+ef_cos = [[4000 * 10 for t in T] for l in L]  #10 hras  ->  #kg/día                       #lt
 ef_jorn = 700 #kg/dia                                           #lt
 ef_cuad = [[5 * ef_jorn for t in T] for l in L]  #Cuadrillas de 5 personas                #lt
 ###############################################################################
@@ -52,10 +52,10 @@ DI = load_DI() #tn
 
 
 #p
-cap_proc = [150, 65, 100, 90, 120] #[P1, P2, P3, P4, P5] tn/dia
-cap_fermentacion = [2500, 1250, 3500, 950, 900] #[P1, P2, P3, P4, P5] tn
-costo_fijo = [27000, 18000, 18700, 22500, 31500] #UTM
-costo_procesado = [20.0, 30.8, 20.8, 27.8, 29.2] #Ch$ /kg
+cap_proc = [150, 65, 100, 90, 120]  #[P1, P2, P3, P4, P5] tn/dia
+cap_fermentacion = [2500, 1250, 3500, 950, 900]  #[P1, P2, P3, P4, P5] tn
+costo_fijo = [27000, 18000, 18700, 22500, 31500]  #UTM
+costo_procesado = [20.0, 30.8, 20.8, 27.8, 29.2]  #Ch$ /kg
 
 
 
@@ -65,14 +65,14 @@ cap_bines = [36, 36, 36, 36, 36, 36, 36, 32, 32, 32, 36, 36, 36, 36, 36, 36, 36,
 12, 12, 12, 12, 12, 12] 
 cap_tolva = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1]
 costo_camion = [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.025, 0.025, 0.025, 0.018, 0.018, 
-0.018, 0.018, 0.018, 0.018, 0.018, 0.018, 0.032, 0.032, 0.032, 0.032, 0.032, 0.032, 0.032] #UTM/TKM
+0.018, 0.018, 0.018, 0.018, 0.018, 0.018, 0.032, 0.032, 0.032, 0.032, 0.032, 0.032, 0.032]  #UTM/TKM
 
 
 cap_montacargas = 22
 kg_bin = 486
 kg_tolva = 10000
 cantidad_tolvas = 100
-CFD = 10 #costo fijo diario
+CFD = 10  #costo fijo diario
 penalizacion = 10
 
 
